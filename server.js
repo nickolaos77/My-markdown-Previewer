66c6267983439cfdc6e7652c9d3ff85e68cb21b8
Nick Papasavvas <nikospapasavvas@gmail.com>
2017-01-31 09:56:57 +0200

    PORT changed

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const app = express();
 
-const PORT = 3000 | process.env.PORT ;
+PORT         = process.env.PORT || 3000;
 app.use(express.static('public'));
 
 app.listen(PORT, function portListener (){
